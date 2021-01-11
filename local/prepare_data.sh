603ac3be30e83c1640d9da70036014efd4f965f7
xun <gongxuncd@gmail.com>
2021-01-11 15:24:46 +0800

    quick fixed copy dataset

--- local/prepare_data.sh
+++ local/prepare_data.sh
@@ -3,11 +3,14 @@
 # Copyright 2020 Audio, Speech and Language Processing Group @ NWPU (Author: Xian Shi)
 # Apache 2.0
 
-original_data_dir=/mnt/lustre/sjtu/users/yzl23/work_dir/local_data/is20_accent_aispeech/is20_AESRC_train
+AESRC_train=/mnt/lustre/sjtu/users/yzl23/work_dir/local_data/is20_accent_aispeech/is20_AESRC_train
+AESRC_eval=/mnt/lustre/sjtu/users/yzl23/work_dir/local_data/is20_accent_aispeech/eval_data
+LIBRISPEECH=/mnt/lustre/sjtu/home/yfc07/workdir/data/librispeech
 
-data_dir=`pwd`/data/
-train_dir=$data_dir/accent_train
-eval_dir=$data_dir/accent_test
+data_dir=`pwd`/data
+train_dir=$data_dir/accent_train # ${train_dir}/origin ->> original data
+eval_dir=$data_dir/accent_test # ${eval_dir}/origin
+librespeech_train_dir=$data_dir/librespeech_train
 
 data=$2         # data transformed into kaldi format
 
@@ -17,43 +20,53 @@ nj=50
 
 vocab_size=1000
 
-# unzip and rename each accent
 if [ $stage -le 0 ];then
-    echo "Unzip and rename data from accent aesrc2020"
-    if [[ ! -r $original_data_dir/AESRC2020.zip ]]; then
-        unzip -q $original_data_dir/AESRC2020.zip -d $data_dir/accent_train
+    echo "unzip and rename each accent aesrc2020"
+    if [[ -w $AESRC_train && -r $AESRC_train/AESRC2020.zip ]]; then
+        unzip -q $AESRC_train/AESRC2020.zip -d $AESRC_train/
     fi
 
-    raw_data=$data_dir/accent_train
-    if [[ ! -e $data_dir/accent_train/US ]]; then
-        mv $raw_data/American\ English\ Speech\ Data $raw_data/US
-        mv $raw_data/British\ English\ Speech\ Data $raw_data/UK
-        mv $raw_data/Chinese\ Speaking\ English\ Speech\ Data $raw_data/CHN 
-        mv $raw_data/Indian\ English\ Speech\ Data $raw_data/IND 
-        mv $raw_data/Portuguese\ Speaking\ English\ Speech\ Data $raw_data/PT 
-        mv $raw_data/Russian\ Speaking\ English\ Speech\ Data $raw_data/RU 
-        mv $raw_data/Japanese\ Speaking\ English\ Speech\ Data $raw_data/JPN 
-        mv $raw_data/Korean\ Speaking\ English\ Speech\ Data $raw_data/KR
-    fi
+    # Train Set
+    mkdir -p ${train_dir}/origin
+    ln -fns ${AESRC_train}/American\ English\ Speech\ Data ${train_dir}/origin/US
+    ln -fns ${AESRC_train}/British\ English\ Speech\ Data ${train_dir}/origin/UK
+    ln -fns ${AESRC_train}/Chinese\ Speaking\ English\ Speech\ Data ${train_dir}/origin/CHN 
+    ln -fns ${AESRC_train}/Indian\ English\ Speech\ Data ${train_dir}/origin/IND 
+    ln -fns ${AESRC_train}/Portuguese\ Speaking\ English\ Speech\ Data ${train_dir}/origin/PT 
+    ln -fns ${AESRC_train}/Russian\ Speaking\ English\ Speech\ Data ${train_dir}/origin/RU 
+    ln -fns ${AESRC_train}/Japanese\ Speaking\ English\ Speech\ Data ${train_dir}/origin/JPN 
+    ln -fns ${AESRC_train}/Korean\ Speaking\ English\ Speech\ Data ${train_dir}/origin/KR
 
-    unset raw_data
-fi
+    # Test Set
+    mkdir -p ${eval_dir}/origin
+    ln -fns ${AESRC_eval} ${eval_dir}/origin
 
-# generate kaldi format data for all
-if [ $stage -le 2 ];then 
+    # Librespeech
+    # mkdir -p ${librespeech_train_dir}
+    ln -fns ${LIBRISPEECH} ${librespeech_train_dir}
+
+    # generate kaldi format data for all
     echo "Generating kaldi format data."
-    find $train_dir -name '*.wav' > $train_dir/wavpath
-    awk -F'/' '{print $(NF-2)"-"$(NF-1)"-"$NF}' $data/data_all/wavpath | sed 's:\.wav::g' > $data/data_all/uttlist
-    paste $data/data_all/uttlist $data/data_all/wavpath > $data/data_all/wav.scp
-    python local/tools/preprocess.py $data/data_all/wav.scp $data/data_all/trans $data/data_all/utt2spk # faster than for in shell
-    ./utils/utt2spk_to_spk2utt.pl $data/data_all/utt2spk > $data/data_all/spk2utt
+    for work_dir in ($train_dir $eval_dir); do
+        find -L ${work_dir}/origin -name '*.wav' > ${work_dir}/wavpath
+        
+        awk -F'/' '{print $(NF-2)"-"$(NF-1)"-"$NF}' ${work_dir}/wavpath | \
+            sed 's:\.wav::g' \
+            > ${work_dir}/uttlist
+        paste ${work_dir}/uttlist ${work_dir}/wavpath > ${work_dir}/wav.scp
+
+        python local/tools/preprocess.py ${work_dir}/wav.scp ${work_dir}/trans ${work_dir}/utt2spk # faster than for in shell
+        ./utils/utt2spk_to_spk2utt.pl ${work_dir}/utt2spk > ${work_dir}/spk2utt
+    done
 fi
 
+exit 1
 
 # clean transcription
-if [ $stage -le 3 ];then
+if [ $stage -le 1 ];then
     echo "Cleaning transcription."
     tr '[a-z]' '[A-Z]' < $data/data_all/trans > $data/data_all/trans_upper
+    
     # turn "." in specific abbreviations into "<m>" tag
     sed -i -e 's: MR\.: MR<m>:g' -e 's: MRS\.: MRS<m>:g' -e 's: MS\.: MS<m>:g' \
         -e 's:^MR\.:MR<m>:g' -e 's:^MRS\.:MRS<m>:g' -e 's:^MS\.:MS<m>:g' $data/data_all/trans_upper 
@@ -68,7 +81,6 @@ if [ $stage -le 3 ];then
     paste $data/data_all/uttlist $data/data_all/trans_upper > $data/data_all/text
 fi
 
-exit 1;
 # extracting filter-bank features and cmvn
 if [ $stage -le 4 ];then 
     ./utils/fix_data_dir.sh $data/data_all
@@ -76,7 +88,6 @@ if [ $stage -le 4 ];then
     ./steps/compute_cmvn_stats.sh $data/data_all $data/feats/log $data/feats/ark # for kaldi 
 fi
 
-exit 1;
 # divide development set for cross validation
 if [ $stage -le 5 ];then 
     for i in US UK IND CHN JPN PT RU KR;do 
